60a8136829c9fea896809455a801209b6d392725
Gyanaranjan Mallick <gyanaranjanmallick444@gmail.com>
2025-05-07 12:49:36 +0530

    Create variables.tf

---
+++ ror-app/modules/ecr/variables.tf
@@ -0,0 +1,9 @@
+variable "aws_region" {
+  description = "AWS region to deploy resources into"
+  type        = string
+}
+
+variable "name" {
+  description = "Name of the ECR repository"
+  type        = string
+}
