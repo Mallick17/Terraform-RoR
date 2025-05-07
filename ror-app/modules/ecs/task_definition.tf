99a1c47a2bb5276fc0b49a6588db687e5f7956e0
Gyanaranjan Mallick <gyanaranjanmallick444@gmail.com>
2025-05-07 12:44:19 +0530

    Create task_definition.tf

---
+++ ror-app/modules/ecs/task_definition.tf
@@ -0,0 +1,74 @@
+resource "aws_ecs_cluster" "this" {
+  name = var.cluster_name
+}
+
+resource "aws_ecs_task_definition" "this" {
+  family                   = "final-ror-task-definition"
+  requires_compatibilities = ["EC2"]
+  network_mode             = "bridge"
+  cpu                      = "2048"
+  memory                   = "3072"
+  execution_role_arn       = "arn:aws:iam::339713104321:role/ecsTaskExecutionRole"
+
+  container_definitions = jsonencode([
+    {
+      name      = "app",
+      image     = var.image_url,
+      essential = true,
+      cpu       = 512,
+      memoryReservation = 512,
+      memory    = 1024,
+      portMappings = [
+        {
+          containerPort = 80,
+          hostPort      = 80,
+          protocol      = "tcp"
+        }
+      ],
+      environment = [
+        { name = "RAILS_ENV",         value = "production" },
+        { name = "DB_USER",           value = "myuser" },
+        { name = "DB_PASSWORD",       value = "mypassword" },
+        { name = "DB_HOST",           value = "rorchatapp.c342ea4cs6ny.ap-south-1.rds.amazonaws.com" },
+        { name = "DB_PORT",           value = "5432" },
+        { name = "DB_NAME",           value = "rorchatapp" },
+        { name = "REDIS_URL",         value = "redis://redis:6379/0" },
+        { name = "RAILS_MASTER_KEY",  value = "c3ca922688d4bf22ac7fe38430dd8849" },
+        { name = "SECRET_KEY_BASE",   value = "600f21de02355f788c759ff862a2cb22ba84ccbf072487992f4c2c49ae260f87c7593a1f5f6cf2e45457c76994779a8b30014ee9597e35a2818ca91e33bb7233" }
+      ],
+      dependsOn = [
+        {
+          containerName = "redis",
+          condition     = "START"
+        }
+      ]
+    },
+    {
+      name      = "redis",
+      image     = "redis:7",
+      essential = false,
+      cpu       = 256,
+      memory    = 512,
+      portMappings = [
+        {
+          containerPort = 6379,
+          hostPort      = 6379,
+          protocol      = "tcp"
+        }
+      ]
+    }
+  ])
+}
+
+resource "aws_ecs_service" "this" {
+  name            = "${var.cluster_name}-service"
+  cluster         = aws_ecs_cluster.this.id
+  task_definition = aws_ecs_task_definition.this.arn
+  desired_count   = 1
+  launch_type     = "EC2"
+
+  depends_on = [
+    aws_ecs_cluster.this,
+    aws_ecs_task_definition.this
+  ]
+}
